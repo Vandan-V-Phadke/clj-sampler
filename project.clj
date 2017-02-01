e1032ffbc5429af08f863b600391606b28f59af4
Vandan Phadke <vandan.p@endurance.com>
2017-02-02 00:25:09 +0530

    Added incanter reader

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject clj-sampler "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "This is a data sampling library in Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [incanter "1.5.7"]]
   :main ^:skip-aot clj-sampler.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clj_sampler/data.clj
+++ src/clj_sampler/data.clj
@@ -2,9 +2,9 @@
   (:require [clojure.java.io :as io]
             [incanter.io :as reader]))
 
-(defmulti load-data identity)
-
-(defmethod load-data :incanter [file_path]
+(defn load-incanter-data [file_path]
   (-> (io/resource file_path)
       (str)
       (reader/read-dataset :header true)))
+
+(load-incanter-data "iris.csv")
